a2cf4f29a8b46c1311557bbb85a4fe212795611a
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-22 13:42:13 +0300

    Initial Commit

---
+++ HW6 - Random Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6 - Random Contact List
+//
+//  Created by Сергей Ткаченко on 22.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
